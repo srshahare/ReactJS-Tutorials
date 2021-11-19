b7de3d9fffc8523383b5db6b05e3b058c3a985da
srshahare <srshahare22@gmail.com>
2021-11-19 14:27:31 +0530

    first tutorial

---
+++ src/Exp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Exp.css'
+
+const Exp = () => {
+    return (
+        <div>
+            <h2 contentEditable={true} >Text</h2>
+        </div>
+    )
+}
+
+export default Exp
